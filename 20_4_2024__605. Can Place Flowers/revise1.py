8ae9bc248adc989b54d0eab6f4f7759b3ff00435
CONSTANT1386 <142436322+CONSTANT1386@users.noreply.github.com>
2024-04-20 14:57:44 +0800

    Update revise1.py

--- 20_4_2024__605. Can Place Flowers/revise1.py
+++ 20_4_2024__605. Can Place Flowers/revise1.py
@@ -1,1 +1,19 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
 
+        flowerbed = [0] + flowerbed  
+        N = len(flowerbed)
+        i = 1
+        while i < N:
+            if sum(flowerbed[i-1:i+2]) == 0:
+                n -= 1
+                i += 1
+            if n <= 0:
+                return True
+            i += 1
+    
+        return False
+
+
+# IDEA: check every 3 item as a group
+# optimization: return immediately as the n reach zero, reduce the following calculation
