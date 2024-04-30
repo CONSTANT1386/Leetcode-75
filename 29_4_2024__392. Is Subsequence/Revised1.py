f5c9f375c4ed81205f81d31175d3731f6d8a292c
CONSTANT1386 <142436322+CONSTANT1386@users.noreply.github.com>
2024-04-30 16:45:43 +0800

    Update Revised1.py

--- 29_4_2024__392. Is Subsequence/Revised1.py
+++ 29_4_2024__392. Is Subsequence/Revised1.py
@@ -1,7 +1,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
+        # if not s:
+        #    return True
         n,m = len(s), len(t)
         i = j = 0
         while i<n and j<m:
