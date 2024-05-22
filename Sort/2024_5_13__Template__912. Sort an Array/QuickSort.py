eb73f2cad5db545d1ae589d231ac8e566622c7a1
CONSTANT1386 <142436322+CONSTANT1386@users.noreply.github.com>
2024-05-22 13:09:58 +0800

    Update QuickSort.py

--- Sort/2024_5_13__Template__912. Sort an Array/QuickSort.py
+++ Sort/2024_5_13__Template__912. Sort an Array/QuickSort.py
@@ -21,5 +21,5 @@ class Solution:
             while i<j and nums[j]>=nums[l]: j -= 1
             while i<j and nums[i]<=nums[l]: i += 1
             nums[i],nums[j] = nums[j],nums[i]
-        nums[i], nums[l] = nums[l], nums[j]
+        nums[i], nums[l] = nums[l], nums[i]        # in this method, the i and j point to a same element at last moment
         return i
