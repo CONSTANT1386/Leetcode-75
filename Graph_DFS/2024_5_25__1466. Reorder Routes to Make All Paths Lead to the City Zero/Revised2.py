a78400327de04df92de80c782dcc4cb600a9e6f8
CONSTANT1386 <142436322+CONSTANT1386@users.noreply.github.com>
2024-05-25 15:34:43 +0800

    Update Revised2.py

--- Graph_DFS/2024_5_25__1466. Reorder Routes to Make All Paths Lead to the City Zero/Revised2.py
+++ Graph_DFS/2024_5_25__1466. Reorder Routes to Make All Paths Lead to the City Zero/Revised2.py
@@ -1,11 +1,14 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        # Initialize the adjacency list for each node
         edges = [[] for _ in range(n)]
         for a, b in connections:
-            edges[a].append((b,1))
-            edges[b].append((a,0))
+            edges[a].append((b,1))        # outgoing edge from a to b (direction needs to change)
+            edges[b].append((a,0))        # incoming edge from b to a (direction is correct)
+            
         count = 0
         stack = [(0,-1)]
+         # Perform a DFS using stack
         while stack:
             cur, parent = stack.pop()
             for nei, direction in edges[cur]:
